c596f0151258f41c421433a089c943810c153f00
Alex Rock Ancelet <pierstoval@gmail.com>
2016-11-23 11:43:00 +0100

    Add sleep 1second for touchpad to initialize properly

--- bin/touchpad-switcher.sh
+++ bin/touchpad-switcher.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-touchpad_id=$(xinput | grep -i "touchpad" | cut -f2 | cut -d '=' -f2);
+sleep 1
 
+touchpad_id=$(xinput | grep -i "touchpad" | cut -f2 | cut -d '=' -f2);
 
 if xinput | grep -i "mouse" | grep -i "pointer"
 
@@ -10,6 +11,7 @@ if xinput | grep -i "mouse" | grep -i "pointer"
 
     else xinput set-prop $touchpad_id "Device Enabled" 1 |
          notify-send "The touchpad is now enabled." ""
+
 
 fi
 
