ddc50385f9b9e02a9e35a4410c5c5b9d42513c5d
Alex Rock Ancelet <pierstoval@gmail.com>
2018-08-31 09:44:06 +0200

    Update/add windows binaries

---
+++ windows_bin/timestamp.bat
@@ -0,0 +1,5 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
+set mytime=%mytime: =% 
+echo %mydate%_%mytime%
--- windows_bin/up.bat
+++ windows_bin/up.bat
@@ -6,10 +6,10 @@ for /f %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set GITBRANCH=%
 
 REM Bash prompt look & colors
 
-set PS1=$E[0;32m$E[0m$E[0;33m$P$E[0m
+set PS1=$E[0;36m[$T$H$H$H]$E[0m$s$E[0;32m$E[0m$E[0;33m$P$E[0m$E[0;32m$s
 
 if not "%GITBRANCH%" == "" (
-    set PS1=%PS1% $E[0;36m$C%GITBRANCH%$F$E[0m
+    set PS1=%PS1%$E[0;36m$C%GITBRANCH%$F$E[0m$s
 )
 
 REM Dollar (or sharp) will change color depending on last exit code
@@ -19,6 +19,6 @@ if errorlevel 0 (
     set PS1=%PS1%$E[0;31m
 )
 
-set PS1=%PS1% $$$E[0m 
+set PS1=%PS1%$$$E[0m 
 
 prompt %PS1%
