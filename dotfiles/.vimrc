23da3565532a6c98c21cf4fdc72030e4f06c279b
Alex Rock Ancelet <pierstoval@gmail.com>
2018-11-20 21:12:07 +0100

    Revamp vim conf and installation process

--- dotfiles/.vimrc
+++ dotfiles/.vimrc
@@ -1,6 +1,37 @@
+" Plugins to install:
+" git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+set hidden
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" Plugins load STARTS HERE
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'scrooloose/nerdtree'
+Plugin 'nelsyeung/twig.vim'
+Plugin 'vim-airline/vim-airline'
+Plugin 'vim-airline/vim-airline-themes'
+Plugin 'vim-ctrlspace/vim-ctrlspace'
+
+
+" Plugins load ENDS HERE
+call vundle#end()            " required
+filetype plugin indent on    " required
+
+let g:vundle_default_git_proto = 'git'
+
+
+"
+" Custom config not related to plugins
+"
 
 " Automatic syntax highlighting if available
 syntax on
+
 
 " Show line numbers
 set number
--- install.sh
+++ install.sh
@@ -1,67 +1,17 @@
 #!/bin/bash
 
 echo "WARNING!"
-echo "This can override all softwares you intend to install,"
+echo "This can (and WILL) override all softwares you intend to install,"
 echo "bash_aliases, vimrc, git config files, etc., so be sure of"
 echo "what you are doing!"
 
-# Get base user
-BASE_USER=`whoami`
-
-BASE_URL="https://raw.githubusercontent.com/Pierstoval/dotfiles/master"
-
-
-# Match case insensitive
-shopt -s nocasematch
-
-# Read answer
-read -rsp $'Continue? [Y/n] > ' -n1 key
-
-if [[ ${key} =~ "y" ]]
-then
-    echo "Processing!"
-else
-    echo "Ok, bye then!"
-    exit 1
+if [[ $(which git) == "" ]];
+    sudo apt install git
 fi
 
-echo "Home dir is ${HOME}"
-echo ""
-echo "------------------------------"
-echo "Installing config files"
+git clone git@github.com:Pierstoval/dotfiles.git ~/dotfiles
 
-files=(
-    'bash_aliases'
-    'gitconfig'
-    'gitignore'
-    'inputrc'
-    'tmux.conf'
-    'vimrc'
-)
-
-for FILE in "${files[@]}"
-do
-    echo -e "Downloading file .${FILE}"
-
-    # Read answer
-    read -rsp $" Continue? [y/N] > " -n1 key
-
-    if [[ ${key} =~ "y" ]]; then
-        wget --quiet -O "${HOME}/.${FILE}" "${BASE_URL}/dotfiles/${FILE}" > /dev/null 2>>install.log
-        if [ -f "${HOME}/.${FILE}" ];
-        then
-           echo " Ok !"
-        else
-           echo "ERR > File .${FILE} could not be downloaded..."
-        fi
-    else
-        echo "Skip"
-    fi
-done
-
-
-echo "------------------------------"
-echo "Making sure that config files are loaded on shell startup"
+mkdir -p ~/bin
 
 # Create bashrc if not exists
 [[ -f ~/.bashrc ]] || touch ~/.bashrc
@@ -69,92 +19,13 @@ echo "Making sure that config files are loaded on shell startup"
 # Add source bash_aliases if not present in bashrc
 (cat ~/.bashrc | grep "bash_aliases") > /dev/null 2>&1 || echo "source ~/.bash_aliases" >> ~/.bashrc
 
+# Copy dotfiles
+cp -r ~/dotfiles/* ~/
+cp -r ~/bin/* ~/bin/
+cp -r ~/etc/* /etc/
 
-
-
-
-
-
-echo "------------------------------"
-echo "Installing binaries (some may need updates)"
-
-# Create $HOME/bin if not exists
-[[ -d "${HOME}/bin" ]] || mkdir "${HOME}/bin"
-
-files=(
-    'aptupgrade'
-    'behat'
-    'box'
-    'composer'
-    'deprecation-detector'
-    'docker-compose'
-    'git-squash'
-    'melody'
-    'php-cs-fixer'
-    'phpunit'
-    'sensiocloud'
-    'symfony'
-    'touchpad-switcher.sh'
-)
-
-read -rsp $" Download binaries? [Y/n] > " -n1 key
-
-if [[ ${key} =~ "y" ]]; then
-    for FILE in "${files[@]}"
-    do
-        echo -e "Downloading binary ${FILE}\c"
-        wget --quiet -O "${HOME}/bin/${FILE}" "${BASE_URL}/bin/${FILE}" > /dev/null 2>>install.log
-        if [ -f "${HOME}/bin/${FILE}" ];
-        then
-           echo " > Ok !"
-        else
-           echo "\nERR > Binary ${FILE} could not be downloaded..."
-        fi
-
-        chmod a+x ${HOME}/bin/${FILE}
-    done
-else
-    echo "Skip"
-fi
-
-
-
-echo "Downloading other needed files"
-
-download_file() {
-    sourcefile=$1
-    destination=$2
-
-    if [[ ${key} =~ "y" ]]; then
-        for sourcefile in "${files[@]}"
-        do
-            if [[ $destination == "" ]]; then
-                destination="${sourcefile}"
-            else
-                destination="${destination}/$(basename sourcefile)"
-            fi
-
-            echo -e "Downloading file ${sourcefile}\c"
-            sudo wget --quiet -O "${destination}" "${BASE_URL}/${sourcefile}" > /dev/null 2>>install.log
-            if [ -f "${sourcefile}" ];
-            then
-               echo " > Ok !"
-            else
-               echo "\nERR > File ${sourcefile} could not be downloaded..."
-            fi
-        done
-    else
-        echo "Skip"
-    fi
-}
-
-download_file "/etc/dnsmasq.d/localhost.dev"
-
-php_conf_dir="$(php -i | grep 'Scan this dir for additional .ini files' | grep -oP '(?<=\=\>\s).*')"
-
-if [[ $php_conf_dir != "" ]]; then
-    download_file "/etc/php/99-custom.ini" $php_conf_dir
-fi
+# Install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "------------------------------"
 echo "Finished!"
