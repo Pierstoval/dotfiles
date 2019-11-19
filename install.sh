95cd7142125c8ba8a64a81123f6cbe3a1ee8381f
Alex Rock <pierstoval@gmail.com>
2019-11-19 18:30:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ git clone git@github.com:Pierstoval/dotfiles.git ~/dotfiles
 mkdir -p ~/bin
 
 # Create bashrc if not exists
-[[ -f ~/dotfiles/.bashrc ]] || touch ~/dotfiles/.bashrc
+[[ -f ~/.bashrc ]] || touch ~/.bashrc
 
 # Add source bash_aliases if not present in bashrc
 (cat ~/.bashrc | grep "bash_aliases") > /dev/null 2>&1 || echo "source ~/.bash_aliases" >> ~/.bashrc
