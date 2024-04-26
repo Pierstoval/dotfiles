634b84006a6bfb061e2b7e66430f2ca81fef2242
Alex Rock Ancelet <pierstoval@gmail.com>
2024-04-26 20:17:29 +0200

    Update the global script to separate distant and local install

--- install.sh
+++ install.sh
@@ -12,32 +12,6 @@ fi
 
 git clone https://github.com/Pierstoval/dotfiles "${HOME}/dotfiles"
 
-# Bash
-if [[ "${SHELL}" == "/bin/bash" ]]; then
-  # Create bashrc if it does not exist
-  [[ -f "${HOME}/.bashrc" ]] || touch "${HOME}/.bashrc"
+cd "${HOME}/dotfiles"
 
-  # Add source bash_aliases if not present in bashrc
-  (cat "${HOME}/.bashrc" | grep "bash_aliases") > /dev/null 2>&1 || echo "source ${HOME}/.bash_aliases" >> "${HOME}/.bashrc"
-
-  (cat "${HOME}/.bashrc" | grep "bash_aliases") > /dev/null 2>&1 || echo "source ./post-shell-start.bash" >> "${HOME}/.bashrc"
-fi
-
-# Zsh
-if [[ "${SHELL}" == "/usr/bin/zsh" ]]; then
-  if [[ ! -f "${HOME}/.zshrc" ]]; then
-      touch "${HOME}/.zshrc"
-  fi
-  (cat "${HOME}/.bashrc" | grep "bash_aliases") > /dev/null 2>&1 || echo "source ./post-shell-start.bash" >> "${HOME}/.zshrc"
-fi
-
-# Copy dotfiles
-cp -r "${HOME}"/dotfiles/dotfiles/* "${HOME}/"
-cp -r "${HOME}"/dotfiles/dotfiles/.* "${HOME}/"
-cp -r "${HOME}"/dotfiles/bin/* "${HOME}/bin/"
-
-echo "------------------------------"
-echo "Finished!"
-echo ""
-echo "Now you should load config file in current environment by executing this command:"
-echo "source ~/.bash_aliases"
+bash ./install_local.sh
--- install_local.sh
+++ install_local.sh
@@ -12,19 +12,32 @@ fi
 
 mkdir -p ~/bin
 
-# Create bashrc if not exists
-[[ -f ~/.bashrc ]] || touch ~/.bashrc
+# Bash
+if [[ "${SHELL}" == "/bin/bash" ]]; then
+  # Create bashrc if it does not exist
+  [[ -f "${HOME}/.bashrc" ]] || touch "${HOME}/.bashrc"
 
-# Add source bash_aliases if not present in bashrc
-(cat ~/.bashrc | grep "bash_aliases") > /dev/null 2>&1 || echo "source ~/.bash_aliases" >> ~/.bashrc
+  # Add source bash_aliases if not present in bashrc
+  (cat "${HOME}/.bashrc" | grep "bash_aliases") > /dev/null 2>&1 || echo "source ${HOME}/.bash_aliases" >> "${HOME}/.bashrc"
 
-# Copy dotfiles
-cp -r ./dotfiles/.* ~/
-cp -r ./bin/* ~/bin/
+  (cat "${HOME}/.bashrc" | grep "post-shell-start") > /dev/null 2>&1 || echo "source ./post-shell-start.bash" >> "${HOME}/.bashrc"
+fi
+
+# Zsh
+if [[ "${SHELL}" == "/usr/bin/zsh" ]]; then
+  if [[ ! -f "${HOME}/.zshrc" ]]; then
+      touch "${HOME}/.zshrc"
+  fi
+  (cat "${HOME}/.bashrc" | grep "bash_aliases") > /dev/null 2>&1 || echo "source ./post-shell-start.bash" >> "${HOME}/.zshrc"
+fi
+
+# Copy dotfiles
+cp -r "${HOME}"/dotfiles/dotfiles/* "${HOME}/"
+cp -r "${HOME}"/dotfiles/dotfiles/.* "${HOME}/"
+cp -r "${HOME}"/dotfiles/bin/* "${HOME}/bin/"
 
 echo "------------------------------"
 echo "Finished!"
 echo ""
 echo "Now you should load config file in current environment by executing this command:"
 echo "source ~/.bash_aliases"
-
